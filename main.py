cbd2a6f1cbc7d1971a8820cfb0d928c5bc6cc417
TheDextiN <dextinxd5@gmail.com>
2024-04-07 21:13:56 +0530

    impliment

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ API_ID = 28015381  # Your API ID
 API_HASH = "d1be9454b29326ed41b0acdef11d202c"
 BOT_TOKEN = "7088928556:AAFulnC-duhQj2mMgyQSt0vob-KKBz3Gk-0"
 
+CONTROL_BOT_TOKEN = "your_control_bot_token"  # Token of your control bot
+
+
 # IDs of users you don't want to respond to
 EXCLUDED_USER_IDS = [710543063, 6444822565]  # Replace with actual user IDs
 
 # The message you want to send
-RESPONSE_MESSAGE = "Use this bot this bot is down:\n@Terabox_Video_Robot\n@Terabox_Video_Robot\n@Terabox_Video_Robot\n@Terabox_Video_Robot"
+RESPONSE_MESSAGE = "Use this bot this bot is down:\n@TeraboxVideoDownloadRobot\n@TeraboxVideoDownloadRobot"
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
