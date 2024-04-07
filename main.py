7b8f5bd51baa8b4ce426ee3571c6aca45e5317b9
TheDextiN <dextinxd5@gmail.com>
2024-04-07 21:43:41 +0530

    update

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ BOT_TOKEN = "7088928556:AAFulnC-duhQj2mMgyQSt0vob-KKBz3Gk-0"
 EXCLUDED_USER_IDS = [710543063, 6444822565]  # Replace with actual user IDs
 
 # The message you want to send
-RESPONSE_MESSAGE = "Use this bot this bot is down:\n@TeraboxVideoDownloadRobot\n@TeraboxVideoDownloadRobot"
+RESPONSE_MESSAGE = "You have to join this channel first @DextiNBots"
 
 app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
